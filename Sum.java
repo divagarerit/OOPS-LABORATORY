32deaf7dd35f6527ef83e4394002227a778da094
DIVAGAR E <divagar.240075@aids.ritchennai.edu.in>
2025-07-31 14:10:49 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum {
+    public static void main(String[] args) {
+        int sum=0;
+        int i;
+        for(i=0;i<=50;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum of numbers from 1 to 50 is "+sum);
+    }
+}
